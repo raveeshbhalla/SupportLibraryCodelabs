faf30f883b7c12f1b49f7cbd44d2afce18f4ba14
Raveesh Bhalla <raveeshbhalla90@gmail.com>
2015-08-08 03:49:34 +0530

    Finished Phase 2

--- app/src/main/java/in/raveesh/supportlibrarycodelabs/MainActivity.java
+++ app/src/main/java/in/raveesh/supportlibrarycodelabs/MainActivity.java
@@ -2,6 +2,7 @@ package in.raveesh.supportlibrarycodelabs;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +11,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     DrawerLayout mDrawerLayout;
     ActionBarDrawerToggle mDrawerToggle;
     EditText firstName, surName, address;
+    RelativeLayout root;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        root = (RelativeLayout) findViewById(R.id.root);
         firstName = (EditText) findViewById(R.id.firstName);
         surName = (EditText) findViewById(R.id.surName);
         address = (EditText) findViewById(R.id.address);
@@ -78,7 +82,15 @@ public class MainActivity extends AppCompatActivity {
         edit.putString("surname", surName.getText().toString());
         edit.putString("address", address.getText().toString());
         edit.apply();
-        Toast.makeText(this, "Saved Details", Toast.LENGTH_LONG).show();
+        Snackbar.make(root, "Saved Details", Snackbar.LENGTH_LONG)
+                .setAction("Clear", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        clearValues();
+                        setStoredValues();
+                    }
+                })
+                .show();
     }
 
     private void setStoredValues() {
@@ -88,4 +100,12 @@ public class MainActivity extends AppCompatActivity {
         address.setText(prefs.getString("address", ""));
     }
 
+    private void clearValues(){
+        SharedPreferences prefs = this.getSharedPreferences("stored", MODE_PRIVATE);
+        SharedPreferences.Editor edit = prefs.edit();
+        edit.putString("firstname", "");
+        edit.putString("surname", "");
+        edit.putString("address", "");
+        edit.apply();
+    }
 }
